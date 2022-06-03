3d140a4109630e086ac6ea9e0cd7b479fb6835a0
Arian Febrian <arianfebrian46@gmail.com>
2022-06-03 09:44:21 +0700

    initial commit

---
+++ android/app/src/main/kotlin/www/smktelkommlg/sch/id/todo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package www.smktelkommlg.sch.id.todo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Todo());
+}
+
+class Todo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: TodoList());
+  }
+}
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  final List<String> _todoList = <String>[];
+  final TextEditingController _textFieldController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('To-Do List'),backgroundColor: Colors.purple[500],centerTitle: true,),
+      body: ListView(children: _getItems(),padding: EdgeInsets.all(10),),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => _displayDialog(context),
+          tooltip: 'Add Item',
+          backgroundColor: Colors.purple[300],
+          child: Icon(Icons.add)),
+    );
+  }
+
+  void _addTodoItem(String title) {
+
+    setState(() {
+      _todoList.add(title);
+    });
+    _textFieldController.clear();
+  }
+
+  Widget _buildTodoItem(String title) {
+    return ListTile(title: Text(title));
+  }
+
+  Future<Future> _displayDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Add a task to your list'),
+            content: TextField(
+              controller: _textFieldController,
+              decoration: const InputDecoration(hintText: 'Enter task here'),
+            ),
+            actions: <Widget>[
+              // add button
+              FlatButton(
+                child: const Text('ADD'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _addTodoItem(_textFieldController.text);
+                },
+              ),
+              // Cancel button
+              FlatButton(
+                child: const Text('CANCEL'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        }
+    );
+  }
+  List<Widget> _getItems() {
+    final List<Widget> _todoWidgets = <Widget>[];
+    for (String title in _todoList) {
+      _todoWidgets.add(_buildTodoItem(title));
+    }
+    return _todoWidgets;
+  }
+}
